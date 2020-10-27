521ab98e95b9ff3d7775085d8ed148b05be94d58
Osama <oussama2ste@gmail.com>
2020-10-27 18:27:01 +0100

    Dev

--- app/src/main/java/com/RootApplication.java
+++ app/src/main/java/com/RootApplication.java
@@ -2,12 +2,9 @@ package com;
 
 import android.app.Application;
 
-import androidx.annotation.Nullable;
 
 import com.osama.mobioptionsads.MobiInitializationListener;
 import com.osama.mobioptionsads.MobiOptionsAdsInit;
-
-import org.jetbrains.annotations.NotNull;
 
 public class RootApplication extends Application {
 
@@ -16,7 +13,7 @@ public class RootApplication extends Application {
     public static RootApplication rootApplication;
 
 
-    public static synchronized void setupMobiOptionsAds(@NotNull MobiInitializationListener listener) {
+    public static synchronized void setupMobiOptionsAds(MobiInitializationListener listener) {
         if (mobiOptionsAdsInit == null) {
             mobiOptionsAdsInit = new MobiOptionsAdsInit(rootApplication, "TJ6N6Wy8aZsc9oWW92TuXlIZwsGtj7", listener);
         }
