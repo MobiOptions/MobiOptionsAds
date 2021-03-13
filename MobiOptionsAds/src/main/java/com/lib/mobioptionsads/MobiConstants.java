90adc07af1fb40739f83e8d330ad135efc0aec95
anass.tl <anass.sadik@taillislabs.com>
2021-03-13 23:50:21 +0100

    changed server to load balanced

--- MobiOptionsAds/src/main/java/com/lib/mobioptionsads/MobiConstants.java
+++ MobiOptionsAds/src/main/java/com/lib/mobioptionsads/MobiConstants.java
@@ -2,7 +2,7 @@ package com.lib.mobioptionsads;
 
 public final class MobiConstants {
 
-    public static final String MOBI_BASE_URL = "https://api.mobioptions.com/api/";
+    public static final String MOBI_BASE_URL = "http://167.172.110.206/api/";
 
     public static final int TWENTY_HOUR_MILLS = 24 * 60 * 60 * 1000;
     public static final int TIME_UNTIL_THE_APP_IS_STARTED = 5 * 1000;
